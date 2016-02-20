833ad09904207f218d3d0ccaef215e75448eb95a
Dipen Chauhan <get.dipen@gmail.com>
2016-02-20 07:28:04 -0800

    calculated absolute relative error

--- assignment1/serial.c
+++ assignment1/serial.c
@@ -1,6 +1,15 @@
+// Author: Dipen Chauhan
+// Assignment 1: Parallel Numerical Integration with Open MPI
+// Date Due: Wednesday, March 9, 11pm.
+// Description: Find the minimum number of trapezoids that gives a correct 
+// answer in 14 significant digits for the definite integral as described below
+// Calculate the absolute relative true error.
+// 
+
 // integrate cos(x/3) - 2*cos(x/5) + 5*sin(x/4) + 8 from 100 to 600
 // Integrate[8 - 2 Cos[x/5] + Cos[x/3] + 5 Sin[x/4], {x, 100, 600}]
-// 4003.720900151326826592913
+// 4003.7209001513268265929134113327386949159025889356124309131658087
+// 4003.7209001513
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,29 +22,30 @@ int main()
 {
 	double a = 100;	//	Lower Limit
 	double b = 600;	//	Upper Limit
-	double n = 2;	//	Total Trapezoids
+	double n = 1;	//	Total Trapezoids
 	double h, x, sum = 0;
+	double true_value = 4003.72090015132682659;
 
-
-
-
-
-	printf("hello world\n");
-	printf("f(%f) = %f\n", a, f(a));
-	printf("f(%f) = %f\n", b, f(b));
-
-	//	Calculate width of each trapeziod
-	h = (b - a) / n;
-
-	for (double i = 0; i <= n; i++)
+	printf("n, sum, relative true error\n");
+	do
 	{
-		x = a + i * h;
-		sum = sum + f(x); 
-	}
+		//	Calculate width of each trapeziod
+		h = (b - a) / n;
+		sum = 0;
 
-	sum = h * (sum - (f(a) / 2) - (f(b) / 2));
+		for (double i = 0; i <= n; i++)
+		{
+			x = a + i * h;
+			sum = sum + f(x); 
+		}
 
-	printf("Sum of trapeziods = %f\n", sum);
+		sum = h * (sum - (f(a) / 2) - (f(b) / 2));
+
+		printf("%.0f %f %.15f\n", n, sum, (true_value - sum) / true_value);
+
+		n = n * 2;
+
+	} while (fabs((true_value - sum) / true_value) > (0.5 * (10^(-14))));
 
 	return EXIT_SUCCESS;
 }
