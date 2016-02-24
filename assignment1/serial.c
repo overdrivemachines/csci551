66b79a9669a960053d97a72905d61c3dff15211f
Dipen Chauhan <get.dipen@gmail.com>
2016-02-24 12:28:48 -0800

    Displaying errors correctly

--- assignment1/serial.c
+++ assignment1/serial.c
@@ -4,6 +4,7 @@
 // Description: Find the minimum number of trapezoids that gives a correct 
 // answer in 14 significant digits for the definite integral as described below
 // Calculate the absolute relative true error.
+// URL: 
 // 
 
 // integrate cos(x/3) - 2*cos(x/5) + 5*sin(x/4) + 8 from 100 to 600
@@ -25,7 +26,9 @@ int main()
 	double a = 100;	//	Lower Limit
 	double b = 600;	//	Upper Limit
 	double n;	//	Total Trapezoids (to be calculated)
-	double true_value = 4003.7209001513268265;
+	double h, x, sum;
+	double true_value = 4003.7209001513268265;	//	True value of integral (using 20 digits)
+	// 					4003.720900151288788
 
 	// Maxima of 2nd derivative of f(x)
 	// maximize(d/dx(d/dx(cos(x/3)-2 cos(x/5)+5 sin(x/4)+8))) from 100 to 600
@@ -35,8 +38,25 @@ int main()
 
 	n = ceil(sqrt(m2*pow((b-a), 3)/(12*allowed_error)));
 
-	printf("n = %f\n", n);
-	
+	printf("Number of Trapezoids n = %.0f\n", n);
+
+
+	// Calculate width of each trapeziod
+	h = (b - a) / n;
+	sum = 0;
+
+	for (double i = 0; i <= n; i++)
+	{
+		x = a + i * h;
+		sum = sum + f(x);
+	}
+
+	sum = h * (sum - (f(a) / 2) - (f(b) / 2));
+
+	printf("True Value = %.15f\n", true_value);
+	printf("Approximate Value = %.15f\n", sum);
+	printf("True Error = %.15f\n", (true_value - sum));
+	printf("Relative True Error = %.15f\n", (true_value - sum) / true_value);
 
 	return EXIT_SUCCESS;
 }
