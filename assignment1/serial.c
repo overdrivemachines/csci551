8216955d6fecf5a7eade59059f5df73b256f56c4
Dipen Chauhan <get.dipen@gmail.com>
2016-03-08 09:40:57 -0800

    renamed integrate function. Printed results in correct format

--- assignment1/serial.c
+++ assignment1/serial.c
@@ -4,13 +4,13 @@
 // Description: Find the minimum number of trapezoids that gives a correct 
 // answer in 14 significant digits for the definite integral as described below
 // Calculate the absolute relative true error.
-// URL: 
+// URL: http://www.ecst.csuchico.edu/~judyc/1516S-csci551/assignments/02-integration.html
 // 
 
-// integrate cos(x/3) - 2*cos(x/5) + 5*sin(x/4) + 8 from 100 to 600
-// Integrate[8 - 2 Cos[x/5] + Cos[x/3] + 5 Sin[x/4], {x, 100, 600}]
-// 4003.7209001513268265929134113327386949159025889356124309131658087
-// 4003.7209001513
+// Function: integrate cos(x/3) - 2*cos(x/5) + 5*sin(x/4) + 8 from 100 to 600
+// Wolframalpha Input: Integrate[8 - 2 Cos[x/5] + Cos[x/3] + 5 Sin[x/4], {x, 100, 600}]
+// Result: 4003.7209001513268265929134113327386949159025889356124309131658087
+// Result: 4003.7209001513
 
 
 #include <stdio.h>
@@ -18,15 +18,15 @@
 #include <math.h>
 
 double f(double x);
-double integrate(int a, int b, int n);
+double integratef(int a, int b, unsigned int n);
 
 int main()
 {
 	double a = 100;	//	Lower Limit
 	double b = 600;	//	Upper Limit
-	double n = 1;	//	Total Trapezoids (to be calculated)
+	unsigned int n = 1;	//	Total Trapezoids (to be calculated)
 	double sum;
-	double max_arte = 0.5 * pow(10,-14);
+	double max_arte = 0.5 * pow(10,-14);	//	Maximum Absolute Relative True Error
 	double arte;	//	Calculated for every n
 	double true_value = 4003.7209001513268265;	//	True value of integral (using 20 digits)
 
@@ -35,31 +35,31 @@ int main()
 	printf("True Value = %f\n", 4003.7209001513268265);
 	printf("Max. Absolute Relative True Error = %.15f\n", max_arte);
 
-
-
-	// Bruteforce way
+	// Finding Error for different values of n
 	do
 	{
-		sum = integrate(a, b, n);
+		sum = integratef(a, b, n);
 
 		// Calculate absolute relative true error
-		arte = (true_value - sum) / true_value;
+		arte = fabs((true_value - sum) / true_value);
 
-		printf("%.0f %f %5.15f\n", n, sum, arte);
+		printf("%d %f %5.15f\n", n, sum, arte);
 
 		n = n * 2;
 
 		// Loop ends when absolute true error is in range
-	} while (fabs((true_value - sum) / true_value) > max_arte);
+	} while (arte > max_arte);
 
 
 	// Print results
 
-	printf("Number of Trapezoids n = %.0f\n", n);
-	printf("True Value = %.15e\n", true_value);
-	printf("Approximate Value = %.15e\n", sum);
-	printf("True Error = %.15e\n", (true_value - sum));
-	printf("Relative True Error = %.15e\n", (true_value - sum) / true_value);
+	printf("Number of Trapezoids n = %d\n", n);
+	printf("True Value = %.19e\n", true_value);
+	printf("Approximate Value = %.19e\n", sum);
+	printf("True Error = %.19e\n", (true_value - sum));
+	printf("Relative True Error = %.19e\n", (true_value - sum) / true_value);
+	printf("Absolute Relative True Error = %.19e\n", arte);
+	printf("Max Absolute Relative True Error = %.19e\n", max_arte);
 
 	return EXIT_SUCCESS;
 }
@@ -69,7 +69,7 @@ double f(double x)
 	return (cos(x/3) - 2*cos(x/5) + 5*sin(x/4) + 8); 
 }
 
-double integrate(int a, int b, int n)
+double integratef(int a, int b, unsigned int n)
 {
 	double h, x, sum;
 
