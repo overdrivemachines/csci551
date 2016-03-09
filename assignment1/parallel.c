5f4bc7b3153227ed7100ea2229cf01671c9f0152
Dipen Chauhan <get.dipen@gmail.com>
2016-03-08 16:48:38 -0800

    calculating local a, b and n

--- assignment1/parallel.c
+++ assignment1/parallel.c
@@ -4,14 +4,19 @@
 #include <mpi.h>     /* For MPI functions, etc */
 
 double f(double x);
-double integrate(int a, int b, int n);
+double integrate(double a, double b, double n);
 
 int main(void)
 {
 	int comm_sz; /* Number of processes    */
 	int my_rank; /* My process rank        */
-	int iparams[3];
-
+	double a = 100;	//	Lower Limit - will change with user input
+	double b = 600;	//	Upper Limit - will change with user input
+	double n = 33554432;	//	Total Trapezoids - will change with user input
+	int iparams[3]; // Params for Integration: a, b and n
+	double h;	//	Width of each trapezoid
+	double sum;	//	Global sum of all trapezoid areas
+	
 	/* Start up MPI */
 	MPI_Init(NULL, NULL); 
 
@@ -24,14 +29,37 @@ int main(void)
 	if (my_rank == 0)
 	{
 		printf("Enter a, b and n\n");
-		scanf("%d %d %d", &iparams[0], &iparams[1], &iparams[2]);
-
-		// printf("%d %d %d\n", iparams[0], iparams[1], iparams[2]);
-		
+		scanf("%lf %lf %lf", &a, &b, &n);
+		iparams[0] = a;
+		iparams[1] = b;
+		iparams[2] = n;
 	}
 
 	MPI_Bcast(iparams, 3, MPI_INT, 0, MPI_COMM_WORLD);
-	printf("%d: %d %d %d\n", my_rank, iparams[0], iparams[1], iparams[2]);	
+	// printf("%d: %f %f %f\n", my_rank, iparams[0], iparams[1], iparams[2]);
+
+	// Calculate width of each trapezoid
+	h = (b - a) / n;
+
+	double local_a, local_b, local_n, local_sum;
+
+	local_n = h / comm_sz;	// Number of trapezoids for each process
+	local_a = a + my_rank*local_n; // Left boundary for each process
+	local_b = local_a + local_n*h;	// Right boundary for each process
+
+	printf("Process %d: %f - %f\n", my_rank, local_a, local_b);
+
+	local_sum = 0;
+
+
+	MPI_Reduce(&local_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+	if (my_rank == 0)
+	{
+		printf("Total Sum = %f\n", sum);
+	}
+
+	
 
 	/* Shut down MPI */
    MPI_Finalize(); 
@@ -43,7 +71,7 @@ double f(double x)
 	return (cos(x/3) - 2*cos(x/5) + 5*sin(x/4) + 8); 
 }
 
-double integrate(int a, int b, int n)
+double integratef(double a, double b, double n)
 {
 	double h, x, sum;
 
@@ -52,14 +80,23 @@ double integrate(int a, int b, int n)
 	sum = 0;
 
 	// Add the areas of all the trapezoids
-	for (double i = 0; i <= n; i++)
-	{
-		x = a + i * h;
-		sum = sum + f(x); 
-	}
+	// for (double i = 0; i <= n; i++)
+	// {
+	// 	x = a + i * h;
+	// 	sum = sum + f(x); 
+	// }
 
 	// The first and last terms need to be halved
-	sum = h * (sum - (f(a) / 2) - (f(b) / 2));
+	// sum = h * (sum - (f(a) / 2) - (f(b) / 2));
+
+	// Textbook way...
+	sum = (f(a) + f(b)) / 2.0;
+	for (unsigned int i = 1; i <= n-1; i++)
+	{
+		x = a + i * h;
+		sum += f(x); 
+	}
+	sum = h * sum;
 
 	return sum;
 }
