3984f1bea7bda89d68187c64b45ef9f8410799d2
Dipen Chauhan <get.dipen@gmail.com>
2016-04-20 21:34:21 -0700

    filled matrix and vector with random numbers

--- assignment3/ge.c
+++ assignment3/ge.c
@@ -8,16 +8,55 @@ int main(int argc, char const *argv[])
 	int matrixSize = strtol(argv[1], NULL, 10);
 	int thread_count = strtol(argv[2], NULL, 10);
 	float *a;	// n x n Matrix as a 1D array
-	float *b;	// n-vector 
+	float *b;	// n-vector
 
+	int i, j;
+
+	printf("Matrix Size: %d\n", matrixSize);
+	printf("Thread Count: %d\n", thread_count);
 
 	// Allocate memory
 	a = (float *) malloc(sizeof(float) * matrixSize * matrixSize);
 	b = (float *) malloc(sizeof(float) * matrixSize);
 
 
-	printf("Matrix Size: %d\n", matrixSize);
-	printf("Thread Count: %d\n", thread_count);
+	// Fill Matrix A with random numbers
+	for (i = 0; i < matrixSize*matrixSize; ++i)
+	{		
+		if (i % 2 == 0)
+			a[i] = drand48();
+		else
+			a[i] = -1 * drand48();
+	}
+
+	// Fill Vector b with random numbers
+	for (i = 0; i < matrixSize; ++i)
+	{		
+		if (i % 2 == 0)
+			b[i] = drand48();
+		else
+			b[i] = -1 * drand48();
+	}
+
+	// Display Matrix A:
+	for (i = 0; i < matrixSize; ++i)
+	{
+		for (j = 0; j < matrixSize; ++j)
+		{
+			printf("%.6e ", a[i*matrixSize + j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	// Display Vector B:
+	for (i = 0; i < matrixSize; ++i)
+	{
+		printf("%.6e\n", b[i]);
+	}
+
+
+	
 
 	// Free memory
 	free(a);
