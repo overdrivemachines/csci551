55dc18cbbfe69232c7324093c2e958a885803da6
Dipen Chauhan <get.dipen@gmail.com>
2016-04-23 21:30:16 -0700

    made diagonal elements 1

--- assignment3/ge.c
+++ assignment3/ge.c
@@ -1,3 +1,9 @@
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define debug_printf(fmt, ...) \
+            do { if (DEBUG) printf(fmt, __VA_ARGS__); } while (0)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
@@ -33,7 +39,7 @@ int main(int argc, char const *argv[])
 	int thread_count = strtol(argv[2], NULL, 10);
 	double **a;	// n x n Matrix as a 1D array
 	double *bestRow;
-	double diagonalElement;
+	double diagonalElement, bestElement, factor;
 	int bestRowIndex = 0;
 
 	int i, j, k;	// for loop counters
@@ -54,71 +60,80 @@ int main(int argc, char const *argv[])
 		a[i] = (double *) malloc((matrixSize+1) * sizeof(double));
 		for (j = 0; j < matrixSize + 1; ++j)
 		{
-			a[i][j] = -1.0e6 + drand48() * (2.0e6);
+			// generating random number
+			// drand48() returns [0, 1)
+			// a[i][j] = -1.0e6 + drand48() * (2.0e6);
+			// a[i][j] = rand() % 10;
+			scanf("%lf",&a[i][j]);
 		}		
 	}
 
 	// Display augmented matrix:
 	displayAugmentedMatrix(a, matrixSize);
 
-	// For each column, annihilate below the diagonal
-	// for (i = 0; i < matrixSize - 1; ++i)
-	// {
-	// 	// For each row j below row i
-	// 	for (j = i + 1; j < matrixSize; ++j)
-	// 	{
-	// 		temp = a[i*matrixSize + i];
-	// 		for (k = 0; i < matrixSize; ++i)
-	// 		{
-	// 			a[j*matrixSize + k] = a[j*matrixSize + k] - (temp / a[i*matrixSize + i]) * a[i*matrixSize + k];
-	// 		}
-	// 	}
-		
-	// }
-
-
 	for (i = 0; i < matrixSize - 1; ++i)
 	{
 		// Diagonal Element
 		diagonalElement = a[i][i];
-		printf("diagonalElement = %f\n", diagonalElement);
+		debug_printf("diagonalElement%d = %f\n", i, diagonalElement);
 
 		// Find the best row (the one with the largest absolute value in the column being worked on)
 		bestRowIndex = i;
+		bestElement = diagonalElement;
 		for (j = i + 1; j < matrixSize; ++j)
 		{
-			if (fabs(a[j][i]) > fabs(diagonalElement))
+			if (fabs(a[j][i]) > fabs(bestElement))
+			{
 				bestRowIndex = j;
+				bestElement = a[j][i];
+				// debug_printf("bestElement = %f\n", a[j][i]);
+			}
 		}
-
-		// printf("Best row = %d %f\n", bestRowIndex, a[bestRowIndex][i]);
 
 		// Swap the rows
 		if (i != bestRowIndex)
 		{
-			printf("Row %d needs to be swapped with Row %d\n", i, bestRowIndex );
-			// double *temp;
-			// temp = a[i];
-			// a[i] = a[bestRowIndex];
-			// a[bestRowIndex] = temp;
-			swapRow(&a[i], &a[bestRowIndex]);		
+			debug_printf("Row %d needs to be swapped with Row %d\n", i, bestRowIndex );
+			swapRow(&a[i], &a[bestRowIndex]);	
+			// Update the diagonal element
+			diagonalElement = a[i][i];
+			debug_printf("diagonalElement%d = %f\n", i, diagonalElement);
+			displayAugmentedMatrix(a, matrixSize);
 		}
 
-		// for (j = 0; j < matrixSize+1; ++j)
-		// 	a[i*matrixSize + j] = a[i*matrixSize + j] / diagonalElement;
+		// To make the diagonal element 1, divide the whole row with the diagonal element
+		debug_printf("Row %d = Row %d / %f\n", i, i, diagonalElement);
+		for (j = 0; j < matrixSize + 1; ++j)
+		{
+			a[i][j] = a[i][j] / diagonalElement;
+		}
 
-		// printf("diagonalElement = %f\n", diagonalElement);
-		// for (j = i + 1; j < matrixSize; ++j)
-		// {
-		// 	for (k = 0; k < matrixSize + 1; ++k)
-		// 	{
-		// 		// a[j*matrixSize + k] = a[j*matrixSize + k] - ;
-		// 	}
-		// }
+		// Force the diagonal to be 1 (to avoid any roundoff errors in dividing above)
+		// a[i][i] = 1;
+		// diagonalElement = 1;
+
+		// Zero all the elements in the row below the diagonal element
+		for (j = i + 1; j < matrixSize; ++j)
+		{
+			factor = a[j][i];
+			if (factor != 0)
+			{
+				debug_printf("Row %d = Row %d - %f*Row %d\n", j, j, factor, i);
+				for (k = i; k < matrixSize + 1; ++k)
+				{
+					a[j][k] = a[j][k] - factor * a[i][k];
+				}
+				displayAugmentedMatrix(a, matrixSize);
+			}			
+		}
 	}
+
+	a[matrixSize-1][matrixSize] = a[matrixSize-1][matrixSize] / a[matrixSize-1][matrixSize-1];
+	a[matrixSize-1][matrixSize-1] = 1;
 
 	// Display augmented matrix:
 	displayAugmentedMatrix(a, matrixSize);
+
 	
 
 	// Free memory
