68295839bce21d6aabfc0877892729fe16634fa0
Dipen Chauhan <get.dipen@gmail.com>
2016-05-02 23:50:09 -0700

    multiple cores

--- assignment3/submit.sh
+++ assignment3/submit.sh
@@ -3,3 +3,7 @@ rm -f *.o* combined-output.txt combined-error.txt
 # qsub -v OMP_NUM_THREADS=4 -l select=1:ncpus=4 ./run.sh
 qsub ./run.sh -F "1"
 qsub ./run.sh -F "2"
+qsub ./run.sh -F "5"
+qsub ./run.sh -F "10"
+qsub ./run.sh -F "20"
+qsub ./run.sh -F "30"
